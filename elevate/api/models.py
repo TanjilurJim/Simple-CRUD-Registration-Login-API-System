0781969aaed612c5c6e31cfbac6f3750b94549bc
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-09-15 14:54:40 +0600

    installed and setup auth token

--- elevate/api/models.py
+++ elevate/api/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+
+#importing the packages
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+
+
+
 # Create your models here.
 
 class Product(models.Model):
@@ -14,3 +26,12 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name},price ${self.price} "
     
+
+@receiver(post_save, sender=User) 
+def generate_auth_token(sender,instance=None, created = False, **kwargs):
+
+    if created:
+
+        Token.objects.create(user=instance)
+
+
